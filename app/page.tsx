29ec9021a439286f060111f5f67f9bf820e0acb0
Arman Rajib <ahrajib6789@gmail.com>
2025-02-12 01:08:59 +0600

    Add theme-toggle mechanism and button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from '@/components/theme-toggle'
+
 export default function Home() {
   return (
-    <section className='py-24'>
-      <div className='container'>
+    <section className='mx-10 py-5'>
+      <div className='container flex flex-row-reverse items-center justify-between'>
+        <ModeToggle />
         <h1 className='text-3xl font-bold'>Portfolio</h1>
       </div>
     </section>
